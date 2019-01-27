cf3fb856b6d08260271ebe7a5abc8164ccff21aa
geongeorge <geongeorge98@gmail.com>
2019-01-27 14:43:34 +0530

    edited body

--- message.js
+++ message.js
@@ -1,6 +1,8 @@
 const TOKEN = "EAAe4DKO6CD4BAFZBeLv3TdKyTi9aLf6ynSKh31xtQ0sPyR2XoKfcRMDh7kaw8QcBZAFz4mfgtbkmSOx87sZCGZB8VC0LSR3ZB38S0qT5yQjbaGo9oedKhr4RiZAgLmgf1WKNGFiDny2qUY9ICcGFy3E3d46eKIJn9rb2k3g7QmWgZDZD"
 const API_URL = "https://graph.facebook.com/v2.6/me/messages?access_token="+TOKEN
 
+axios = require('axios')
+
 sendMessage = function(senderId, contextId, msg, btn) {
     let data = {
         "recipient":{
