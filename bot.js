fcac634d94e7e12f29953f762722827a2d084bb2
geongeorge <geongeorge98@gmail.com>
2019-01-27 19:37:10 +0530

    added bot js

---
+++ bot.js
@@ -0,0 +1,53 @@
+const TOKEN = "EAAe4DKO6CD4BAK0bdwEX4XgZCppkYFon5Bwfh9TvI87ZCL4Bn9l4c8gx2uK3C8Vvt8iLltF6CNGpaHK7XC47jnlqggAlIZAZANMTWkFZCVF2OXTSVp8Uv0PsM5FA2r3rI4RIDm7E9jZBreP9pxKYXkMZClOIZCdVez4oKF4Wg96nYwZDZD"
+const API_URL = "https://graph.facebook.com/v2.6/me/messages?access_token="+TOKEN
+
+function sendMessage(player, context, message, cta, payload) {
+    var button = {
+        type: "game_play",
+        title: cta
+    };
+
+    if (context) {
+        button.context = context;
+    }
+    if (payload) {
+        button.payload = JSON.stringify(payload)
+    }
+    var messageData = {
+        recipient: {
+            id: player
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: [
+                    {
+                        title: message,
+                        buttons: [button]
+                    }
+                    ]
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+
+}
+
+function callSendAPI(messageData) {
+    var graphApiUrl = 'https://graph.facebook.com/me/messages?access_token='+TOKEN
+    request({
+        url: graphApiUrl,
+        method: "POST",
+        json: true,  
+        body: messageData
+    }, function (error, response, body){
+        console.error('send api returned', 'error', error, 'status code', response.statusCode, 'body', body);
+    });
+}
+
+module.exports.sendMessage = sendMessage
+module.exports.callSendAPI = callSendAPI
